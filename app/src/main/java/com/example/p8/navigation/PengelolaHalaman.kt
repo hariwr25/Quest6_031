5c2386a9ff999514c76656e52b17cdcf4d269cf4
hariwr <hariwr25@gmail.com>
2024-11-22 12:20:31 +0700

    Fix Error dan memanggil val mahasiswaUiState

--- app/src/main/java/com/example/p8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/p8/navigation/PengelolaHalaman.kt
@@ -1,7 +1,13 @@
 package com.example.p8.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.p8.model.Mahasiswa
 import com.example.p8.ui.viewmodel.MahasiswaViewModel
 import com.example.p8.ui.viewmodel.RencanaStudyViewModel
@@ -19,5 +25,12 @@ fun MahasiswaApp(
     krsViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = PengelolaHalaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
 
+    }
 }
