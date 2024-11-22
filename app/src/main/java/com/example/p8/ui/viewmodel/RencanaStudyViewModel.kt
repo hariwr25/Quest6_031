a6775ff4a3103a78be8ccd1c2430b3ee349f9569
hariwr <hariwr25@gmail.com>
2024-11-22 11:39:11 +0700

    Membuat function setMataKuliah,setKelas, saveDataKRS

--- app/src/main/java/com/example/p8/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/p8/ui/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.p8.ui.viewmodel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.p8.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status ->
+            status.copy(
+                mataKuliah = ls[0],
+                kelas = ls[1],
+            )
+        }
+
+    }
 }
