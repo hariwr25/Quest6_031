abe7aa4ce39214487a52329c7aeb495f0bec826e
hariwr <hariwr25@gmail.com>
2024-11-22 10:08:53 +0700

    Menambahkan dan mengatur Column

--- app/src/main/java/com/example/p8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/p8/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,16 @@
 package com.example.p8.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.p8.R
 import com.example.p8.model.Mahasiswa
 import com.example.p8.model.RencanaStudy
 
@@ -19,4 +25,9 @@ fun RencanaStudyView(
     var pilihanKelas by remember { mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropDown, pilihanKelas)
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = colorResource(id = R.color.primary))
+    ) {
+
+    }
     }
