c5b03b794de6995f6cc3a36a8db33cfb08349f23
hariwr <hariwr25@gmail.com>
2024-11-22 08:45:01 +0700

    Menambahkan Object Mata Kuliah

--- app/src/main/java/com/example/p8/data/MataKuliah.kt
+++ app/src/main/java/com/example/p8/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.p8.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision",
+    )
 }
