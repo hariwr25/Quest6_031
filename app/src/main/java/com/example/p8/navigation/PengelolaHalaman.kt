a539b2962d62e5a6b6a4287bf30c2deb7d9f263b
hariwr <hariwr25@gmail.com>
2024-11-22 14:05:52 +0700

    fix PengelolaHalaman

--- app/src/main/java/com/example/p8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/p8/navigation/PengelolaHalaman.kt
@@ -7,17 +7,23 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.p8.model.Mahasiswa
+import com.example.p8.ui.view.screen.MahasiswaFormView
+import com.example.p8.ui.view.screen.RencanaStudyView
+import com.example.p8.ui.view.screen.SplashView
+import com.example.p8.ui.view.screen.TampilHasilView
 import com.example.p8.ui.view.viewmodel.MahasiswaViewModel
 import com.example.p8.ui.view.viewmodel.RencanaStudyViewModel
 
 enum class Halaman {
     Splash,
-    Mahasis,
+    Mahasiswa,
     Matakuliah,
     Tampil
 }
+
 @Composable
 fun MahasiswaApp(
     modifier: Modifier = Modifier,
@@ -30,6 +36,33 @@ fun MahasiswaApp(
         navController = navController,
         startDestination = Halaman.Splash.name,
         modifier = Modifier.padding()
-    ){
+    ) {
+        composable(route = Halaman.Splash.name) {
+            SplashView(onMulaiButton = {
+                navController.navigate(Halaman.Mahasiswa.name)
+            }
+            )
+        }
+        composable(route = Halaman.Mahasiswa.name) {
+            MahasiswaFormView(
+                onSubmitButtonClicked = {
+                    mahasiswaViewModel.saveDataMahasiswa(it)
+                    navController.navigate(Halaman.MataKuliah.name)
+                },
+                onBackButtonClicked = {
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(route = Halaman.Matakuliah.name) {
+            RencanaStudyView (
+                mahasiswa = mahasiswaUiState,
+                onSubmitButtonClicked = {
+                    krsViewModel.saveDataKRS(it)
+                    navController.navigate(Halaman.Tampil.name)
+                },
+                onBackButtonClicked = { navController.popBackStack() }
+            )
+        }
     }
 }
