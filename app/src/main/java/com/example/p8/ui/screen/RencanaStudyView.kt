6f50a49692d03e69bc34050186c743136799383e
hariwr <hariwr25@gmail.com>
2024-11-22 10:29:22 +0700

    Menambahkan HorizontalDivider untuk Pilih Kelas Belajar

--- app/src/main/java/com/example/p8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/p8/ui/screen/RencanaStudyView.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -117,6 +118,15 @@ fun RencanaStudyView(
                         onValueChangedEvent = {
                             chosenDropDown = it
                         }
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Text(text = "Pilih Kelas Belajar", fontWeight = FontWeight.Bold)
+                    Text(
+                        text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Light
                     )
             }
         }
