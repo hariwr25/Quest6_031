dfe7d61ed9f4377a43bedf4d1bbbfb78be2451b8
hariwr <hariwr25@gmail.com>
2024-11-22 10:21:30 +0700

    Menambahkan Box

--- app/src/main/java/com/example/p8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/p8/ui/screen/RencanaStudyView.kt
@@ -2,6 +2,7 @@ package com.example.p8.ui.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -71,6 +76,26 @@ fun RencanaStudyView(
                     fontSize = 15.sp,
                     color = Color.White
                 )
+                Box{
+                    Icon(
+                        imageVector = Icons.Filled.Notifications,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(
+                            topEnd = 15.dp,
+                            topStart = 15.dp
+                        )
+                    )
+                    .fillMaxSize(),
+
+                ){
             }
         }
     }
