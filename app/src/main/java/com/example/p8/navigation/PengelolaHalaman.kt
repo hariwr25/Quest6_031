92de89c04ec9548662429de94469572504e2631c
hariwr <hariwr25@gmail.com>
2024-11-22 11:52:16 +0700

    Menambahkan function MahasiswaApp

--- app/src/main/java/com/example/p8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/p8/navigation/PengelolaHalaman.kt
@@ -1,8 +1,23 @@
 package com.example.p8.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.p8.model.Mahasiswa
+import com.example.p8.ui.viewmodel.MahasiswaViewModel
+import com.example.p8.ui.viewmodel.RencanaStudyViewModel
+
 enum class PengelolaHalaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
+}
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
 }
